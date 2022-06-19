9b303a8eeb1f72f8e0006b508b1f2b762a4b7fce
Mohamed Korany <mohammed.korany.ali@gmail.com>
2022-06-19 19:04:23 +0200

    Initial Commit

---
+++ ElMenus/ElMenusApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElMenusApp.swift
+//  ElMenus
+//
+//  Created by Mohamed Korany on 19/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct ElMenusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
